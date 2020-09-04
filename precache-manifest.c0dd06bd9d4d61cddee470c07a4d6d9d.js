46780f200baaa0925cef4067b7da8e6703e542de
MAHMOUD HUSSAM <mahmoodhussam@gmail.com>
2020-09-04 17:14:56 +0300

    Updates

---
+++ precache-manifest.c0dd06bd9d4d61cddee470c07a4d6d9d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f0c188d392359889d7675e815808d34d",
+    "url": "/ToDoListReact/index.html"
+  },
+  {
+    "revision": "7dcdac231c7a8749a657",
+    "url": "/ToDoListReact/static/css/main.0890a1d5.chunk.css"
+  },
+  {
+    "revision": "d1af70365dbea5b07c96",
+    "url": "/ToDoListReact/static/js/2.2bc27dc1.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoListReact/static/js/2.2bc27dc1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7dcdac231c7a8749a657",
+    "url": "/ToDoListReact/static/js/main.760053f5.chunk.js"
+  },
+  {
+    "revision": "c2686f9d9da73956a800",
+    "url": "/ToDoListReact/static/js/runtime-main.a2110f78.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDoListReact/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "2c3e6019a6a08b739bb607a4fc17ed38",
+    "url": "/ToDoListReact/static/media/trash.2c3e6019.png"
+  }
+]);
